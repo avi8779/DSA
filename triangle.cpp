551d7a56e29756bc643d4971dc4c3db412c0def6
Avinash kumar <avi87793919@gmail.com>
2026-03-26 18:56:18 +0530

    triangle pattern

---
+++ triangle.cpp
@@ -0,0 +1,16 @@
+// Print an Increasing Triangle of Stars 
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n = 5;
+
+    for(int i = 1; i<=n; i++){
+        for(int j = 0; j<i; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
